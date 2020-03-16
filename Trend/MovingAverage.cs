5b373213e4299b54610c932635b99556cbc60465
David Tanderup <31796790+DavidTanderup@users.noreply.github.com>
2020-03-16 08:42:59 -0400

    Add files via upload

---
+++ Trend/MovingAverage.cs
@@ -0,0 +1,45 @@
+﻿
+namespace TechnicalIndicators.Trend
+{
+    public class MovingAverage
+    {
+        /// <summary>
+        /// Finds the Simple Moving Average (SMA) for the closing prices
+        /// </summary>
+        /// <param name="closePrices">Prices from the close of a given period</param>
+        public MovingAverage(decimal[] closePrices)
+        {
+            TwoHundredDay = Average(200, closePrices);
+            OneHundredDay = Average(100, closePrices);
+            FiftyDay = Average(50, closePrices);
+            GoldenCross = TwoHundredDay < FiftyDay;
+            DeathCross = FiftyDay < TwoHundredDay;
+        }
+        /// <summary>
+        /// Finds the Simple Moving Average (SMA) for the closing prices
+        /// </summary>
+        /// <param name="closePrices">Prices from the close of a given period</param>
+
+        public decimal TwoHundredDay { get; }
+        public decimal OneHundredDay { get; }
+        public decimal FiftyDay { get; }
+        public bool GoldenCross { get; }
+        public bool DeathCross { get; }
+
+        /// <summary>
+        /// Method assumes prices are decending (newest to oldest)
+        /// </summary>
+        /// <param name="period">Number of time periods being averaged</param>
+        /// <param name="closePrices">Prices from the close of a given period</param>
+        /// <returns></returns>
+        private decimal Average(int period, decimal[] closePrices)
+        {
+            decimal sum = 0;
+            for (int i = 0; i < period; i++)
+            {
+                sum += closePrices[i];
+            }
+            return sum / (decimal)period;
+        }
+    }
+}
---
+++ Volume/OnBalanceVolume.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechnicalIndicators.Volume
+{
+    public class OnBalanceVolume
+    {
+        public OnBalanceVolume(decimal[] volume, decimal[] closePrice)
+        {
+            OBV = GetOBV(volume, closePrice).OBV;
+            OBVHistory = GetOBV(volume, closePrice).OBVHistory;
+        }
+
+        private OnBalanceVolume() { }
+        public decimal OBV { get; set; }
+        public decimal [] OBVHistory { get; set; }
+
+
+        private OnBalanceVolume GetOBV(decimal[] volume, decimal[] closePrice)
+        {
+            decimal[] history = new decimal[volume.Length];
+            decimal obv = 0m;
+            decimal previousOBV = volume[volume.Length - 1];
+            history[volume.Length-1]= previousOBV;
+            for (int i = volume.Length - 2; i >= 0; i--)
+            {
+                if (closePrice[i] > closePrice[i + 1])
+                {
+                    obv = previousOBV + volume[i];
+                    history[i] = obv;
+                    previousOBV = obv;
+                }
+                else if (closePrice[i] == closePrice[i + 1])
+                {
+                    obv = previousOBV + 0;
+                    history[i] = obv;
+                    previousOBV = obv;
+                }
+                else
+                {
+                    obv = previousOBV - volume[i];
+                    history[i] = obv;
+                    previousOBV = obv;
+                }
+            }
+            return new OnBalanceVolume() { OBVHistory = history, OBV = obv};
+        }
+    }
+}
