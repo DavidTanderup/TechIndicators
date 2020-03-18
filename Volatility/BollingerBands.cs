5f3f5efdef0fae4da03bccf0ac707328f944ed03
David Tanderup <31796790+DavidTanderup@users.noreply.github.com>
2020-03-18 14:47:06 -0400

    Add files via upload

---
+++ Volatility/BollingerBands.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technical_Indicators.Volatility
+{
+    public class BollingerBands
+    {
+        // TODO: https://www.investopedia.com/terms/b/bollingerbands.asp
+    }
+}
