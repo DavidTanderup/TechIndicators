988d5f202f1df33ca56f7d2d03ee0920e7a778aa
David Tanderup <Davetanderup@gmail.com>
2020-03-31 14:02:43 -0400

    Completes comments for B-Bands

--- Volatility/BollingerBands.cs
+++ Volatility/BollingerBands.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using TechnicalIndicators.Trend;
 
 namespace Technical_Indicators.Volatility
@@ -19,50 +17,88 @@ namespace Technical_Indicators.Volatility
         {
             ClosePrices = closePrices;
             MiddleBandArray = new MovingAverage(ClosePrices).GetAverage(20);
-            StandDevArray = GetBands();
+            StandDevArray = GetStanDev();
             UpperBandArray = GetUpper();
             LowerBandArray = GetLower();
             UpperBand = UpperBandArray[0];
             MiddleBand = MiddleBandArray[0];
             LowerBand = LowerBandArray[0];
         }
+        /// <summary>
+        /// Decimal array of Upper Band data points.
+        /// </summary>
         public decimal[] UpperBandArray { get; }
+        /// <summary>
+        /// Decimal array of 20 period SMA data points.
+        /// </summary>
         public decimal[] MiddleBandArray { get; }
+        /// <summary>
+        /// Decimal array of Lower Band data points.
+        /// </summary>
         public decimal[] LowerBandArray { get; }
+        /// <summary>
+        ///  Point plotted two standard deviations positively away from a simple moving average (SMA) of the Asset's price. 
+        /// </summary>
         public decimal UpperBand { get; }
+        /// <summary>
+        /// Simple moving average (SMA) of the Asset's price for 20 periods.
+        /// </summary>
         public decimal MiddleBand { get; }
+        /// <summary>
+        ///  Point plotted two standard deviations negatively away from a simple moving average (SMA) of the Asset's price.
+        /// </summary>
         public decimal LowerBand { get; }
         /// <summary>
         /// Array of Close Prices
         /// </summary>
         private decimal[] ClosePrices { get; }
+        /// <summary>
+        /// Array of standard deviations points.
+        /// </summary>
         private decimal[] StandDevArray { get; }
 
-        // TODO: https://www.investopedia.com/terms/b/bollingerbands.asp
-
+        /// <summary>
+        /// Calculates the Upper Band. 
+        /// </summary>
+        /// <returns>Decimal Array</returns>
         private decimal[] GetUpper()
         {
+            // set array size
             decimal[] upper = new decimal[MiddleBandArray.Length];
             for (int i = 0; i < MiddleBandArray.Length; i++)
             {
+                // upper band = SMA + (StanDev*2)
                 upper[i] = MiddleBandArray[i] + (StandDevArray[i] * 2);
             }
             return upper;
         }
+        /// <summary>
+        /// Calculates the Lower Band.
+        /// </summary>
+        /// <returns>Decimal Array</returns>
         private decimal[] GetLower()
         {
+            // set array size
             decimal[] lower = new decimal[MiddleBandArray.Length];
+
             for (int i = 0; i < MiddleBandArray.Length; i++)
             {
+                // lower band = SMA - (StanDev*2)
                 lower[i] = MiddleBandArray[i] - (StandDevArray[i] * 2);
             }
             return lower;
         }
-        private decimal[] GetBands()
+        /// <summary>
+        /// Gets the calculations of standard deviations for the Close price data set.
+        /// </summary>
+        /// <returns></returns>
+        private decimal[] GetStanDev()
         {
             decimal[] bands = new decimal[MiddleBandArray.Length];
+            // set variable indexs
             var end = ClosePrices.Length - 1;
             var start = end - 20;
+            // calculate standard deviation for each data set.
             for (int i = start; i >= 0; i--)
             {
                 bands[i] = StandardDeviation(ClosePrices[start..end]);
@@ -71,15 +107,23 @@ namespace Technical_Indicators.Volatility
             }
             return bands;
         }
+        /// <summary>
+        /// Calculates the standard deviation for a given data set.
+        /// </summary>
+        /// <param name="vs"></param>
+        /// <returns></returns>
         private decimal StandardDeviation(decimal[] vs)
         {
+            // create the return array
             double[] vs1 = new double[vs.Length];
+            // find the mean
             var mean = vs.Sum() / vs.Length;
-
+            // subtract the value from the mean and square the result.
             for (int i = 0; i < vs.Length; i++)
             {
                 vs1[i] = Math.Pow((double)Math.Abs(mean - vs[i]), 2);
             }
+            // get the squareroot of the mean for the squared results 
             return Convert.ToDecimal(Math.Sqrt(vs1.Sum() / vs1.Length));
         }
 
