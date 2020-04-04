932a578775247c59d4c6a54c19a2ee8276039280
David Tanderup <Davetanderup@gmail.com>
2020-04-04 18:57:38 -0400

    Adds comments and completes the Stochastic Indicator

---
+++ Momentum/StochasticOscillator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechnicalIndicators.Momentum
+{
+    /// <summary>
+    /// A momentum indicator comparing a particular closing price of a security to a range of its prices over a certain period of time.
+    /// </summary>
+    public class StochasticOscillator
+    {
+        /// <summary>
+        /// A momentum indicator that shows the location of the close relative to the high-low range over a set number of periods.
+        /// </summary>
+        /// <param name="highPrices">High price of the asset.</param>
+        /// <param name="lowPrices">Low price of the asset.</param>
+        /// <param name="closePrices">Close price of the asset.</param>
+        public StochasticOscillator(decimal [] highPrices, decimal [] lowPrices,decimal[] closePrices)
+        {
+            StochasticSlowArray = GetStochastic(highPrices, lowPrices, closePrices);
+            StochasticSlow = StochasticSlowArray[0];
+            StochasticFastArray = GetFastStochastic(StochasticSlowArray);
+            StochasticFast = StochasticFastArray[0];
+
+        }
+        /// <summary>
+        /// Array of the "Fast" %D stochastic data points.
+        /// </summary>
+        public decimal [] StochasticFastArray { get; }
+        /// <summary>
+        /// Array of the "Slow" %K stochastic data points.
+        /// </summary>
+        public decimal [] StochasticSlowArray { get; }
+        /// <summary>
+        /// The "Fast" Stochastic (%D) is the 3 period moving average of %K
+        /// </summary>
+        public decimal StochasticFast { get;}
+        /// <summary>
+        /// The "Slow" Stochastic (%K) 
+        /// </summary>
+        public decimal StochasticSlow { get; }
+
+        private decimal [] GetStochastic(decimal [] highPrices, decimal [] lowPrices, decimal [] closePrices)
+        {
+            // %K = (C-L14/H14-L14)*100
+            // define range
+            var end = closePrices.Length - 1;
+            var start = end - 14;
+            decimal[] stochastic = new decimal[closePrices.Length-14];
+            for (int i = stochastic.Length-1; i >= 0; i--)
+            {
+                var high = highPrices[start..end].Max();
+                var low = lowPrices[start..end].Min();
+                stochastic[i] = ((closePrices[i] - low) / (high - low)) * 100;
+                end--;
+                start--;
+            }
+            return stochastic;
+        }
+        private decimal [] GetFastStochastic(decimal[] stochasticSlow)
+        {
+            decimal[] fastStochastic = new decimal[stochasticSlow.Length - 2];
+
+            var end = stochasticSlow.Length - 1;
+            var start = end - 3;
+            for (int i = fastStochastic.Length-2; i >=0; i--)
+            {
+                fastStochastic[i] = stochasticSlow[start..end].Average();
+                end--;
+                start--;
+            }
+            return fastStochastic;
+        }
+    }
+}
